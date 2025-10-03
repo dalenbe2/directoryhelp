efdb19cf0b3b1c7f6ffa20850223c8938496e2b2
David Dalenberg <dalenbe2@gmail.com>
2025-10-03 09:43:42 -0400

    Change file path structure for Ruby

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -13,12 +13,12 @@ workdirDataDavid <- tibble::tribble(
 
 workdirDataRuby <- tibble::tribble(
   ~shortcut,       ~directory,
-  'repository',    'C:/Users/cheungr/Box/Institutional Research/Data Repository',
-  'datareq',    'C:/Users/cheungr/Box/Institutional Research/Data Requests',
-  'retcalc',    'C:/Users/cheungr/Box/Institutional Research/Retention',
-  'ir',         'C:/Users/cheungr/Box/Institutional Research',
-  'box',        'C:/Users/cheungr/Box/Institutional Research/Box',
-  'downloads',  'C:/Users/cheungr/Downloads'
+  'repository',      '~/Library/CloudStorage/Box-Box/Institutional Research/Data Repository',
+  'datareq',         '~/Library/CloudStorage/Box-Box/Institutional Research/Data Requests',
+  'retcalc',         '~/Library/CloudStorage/Box-Box/Institutional Research/Retention',
+  'ir',              '~/Library/CloudStorage/Box-Box/Institutional Research',
+  'box',             '~/Library/CloudStorage/Box-Box',
+  'downloads',       '~/Downloads'
 )
 
 usethis::use_data(workdirDataDavid, workdirDataRuby, overwrite = TRUE, internal=T)
